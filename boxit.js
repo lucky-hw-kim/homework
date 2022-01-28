bbcca0b3c268d828c725178309df83c7855b0ed5
lucky kim <lucky.hw.kim@gmail.com>
2022-01-28 15:13:18 -0800

    initial_commit_hw1

---
+++ boxit.js
@@ -0,0 +1,24 @@
+function drawLine(num) {
+
+    const line = '━'
+
+    return line.repeat(num)
+    
+}
+console.log(drawLine(20))
+
+function drawTopBorder(num) {
+    
+    const line = '━';
+    const leftCorner = 
+    const rightCorner =
+    
+}
+
+function drawMiddleBorder(num) {
+    
+}
+
+function drawBottomBorder(num) {
+    
+}
